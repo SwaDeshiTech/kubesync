ee16b2f7ce2d0942adf9d089016e81a04cd08975
Prabhat Tyagi <harrytyagi2371@gmail.com>
2026-05-24 00:16:02 +0530

    Added CONFIG_FOLDER environment variable to production startup script (#19)

--- profiles/prod/bin/startup.sh
+++ profiles/prod/bin/startup.sh
@@ -8,7 +8,11 @@ APP_ENV="${1:-prod}"
 # Print the value of APP_ENV for debugging purposes
 echo "APP_ENV is set to: $APP_ENV"
 
+# Set config folder path
+CONFIG_FOLDER="/$APP/conf"
+echo "CONFIG_FOLDER is set to: $CONFIG_FOLDER"
+
 chmod +x /"$APP"/kubesync
 
-# Run the Go application and inject APP_ENV
-APP_ENV="$APP_ENV" /"$APP"/kubesync
+# Run the Go application and inject APP_ENV and CONFIG_FOLDER
+APP_ENV="$APP_ENV" CONFIG_FOLDER="$CONFIG_FOLDER" /"$APP"/kubesync
