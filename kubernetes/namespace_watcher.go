47e1fc1eda03a9889e1f25c80d8c2507eec3e90a
Prabhat Tyagi <harrytyagi2371@gmail.com>
2026-05-30 01:25:00 +0530

    Fix startup script (#20)
    * Added CONFIG_FOLDER environment variable to production startup script
    
    * Added namespace watcher reconnection logic with error handling and logging

--- kubernetes/namespace_watcher.go
+++ kubernetes/namespace_watcher.go
@@ -16,20 +16,31 @@ type NameSpaceWatcher struct {
 }
 
 func (namespaceWatcher *NameSpaceWatcher) Watch() {
-	// Watch for changes in namespaces
-	watcher, err := namespaceWatcher.ClientSet.CoreV1().Namespaces().Watch(context.Background(), metav1.ListOptions{})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Loop through the watch events
 	for {
-		event := <-watcher.ResultChan()
-		switch event.Type {
-		case watch.Added:
-			namespaceName := event.Object.(*v1.Namespace).Name
-			go namespaceWatcher.Broker.Publish("namespace", namespaceName)
+		// Watch for changes in namespaces
+		watcher, err := namespaceWatcher.ClientSet.CoreV1().Namespaces().Watch(context.Background(), metav1.ListOptions{})
+		if err != nil {
+			log.Printf("Error watching namespaces: %v. Retrying in 5 seconds...", err)
+			// Retry after 5 seconds
+			continue
 		}
+
+		log.Println("Namespace watcher started successfully")
+
+		// Loop through the watch events
+		for event := range watcher.ResultChan() {
+			switch event.Type {
+			case watch.Added:
+				namespaceName := event.Object.(*v1.Namespace).Name
+				log.Printf("Namespace added: %s", namespaceName)
+				go namespaceWatcher.Broker.Publish("namespace", namespaceName)
+			case watch.Error:
+				log.Printf("Watch error received: %v", event.Object)
+			}
+		}
+
+		log.Println("Namespace watcher channel closed, reconnecting...")
+		watcher.Stop()
 	}
 }
 
